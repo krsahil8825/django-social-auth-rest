58592f539fa7f776623f14a8fbe539e40d2fa6e2
Kumar Sahil <krsahil8825@gmail.com>
2026-06-06 10:49:45 +0530

    refactor: GitHub and Google OAuth configuration and serializers

--- src/django_social_auth_rest/conf.py
+++ src/django_social_auth_rest/conf.py
@@ -26,17 +26,9 @@ SOCIAL_AUTH_STATE_MAX_AGE = getattr(
 # GitHub OAuth configuration
 GITHUB_CLIENT_ID = getattr(django_settings, "GITHUB_CLIENT_ID", None)
 GITHUB_CLIENT_SECRET = getattr(django_settings, "GITHUB_CLIENT_SECRET", None)
-ENABLE_GITHUB_AUTH = getattr(django_settings, "ENABLE_GITHUB_AUTH", True)
-
-if ENABLE_GITHUB_AUTH and (not GITHUB_CLIENT_ID or not GITHUB_CLIENT_SECRET):
-    raise ValueError(
-        "GITHUB_CLIENT_ID and GITHUB_CLIENT_SECRET must be set if ENABLE_GITHUB_AUTH is True"
-    )
+ENABLE_GITHUB_AUTH = True if GITHUB_CLIENT_ID and GITHUB_CLIENT_SECRET else False
 
 
 # Google OAuth configuration
 GOOGLE_CLIENT_ID = getattr(django_settings, "GOOGLE_CLIENT_ID", None)
-ENABLE_GOOGLE_AUTH = getattr(django_settings, "ENABLE_GOOGLE_AUTH", False)
-
-if ENABLE_GOOGLE_AUTH and not GOOGLE_CLIENT_ID:
-    raise ValueError("GOOGLE_CLIENT_ID must be set if ENABLE_GOOGLE_AUTH is True")
+ENABLE_GOOGLE_AUTH = True if GOOGLE_CLIENT_ID else False
