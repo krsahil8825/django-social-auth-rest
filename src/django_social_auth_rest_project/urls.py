d1f984ace9d637d48ca84bac662c1cd56b73d52d
Kumar Sahil <krsahil8825@gmail.com>
2026-06-14 16:04:44 +0530

    feat: add email_linked field and update unique constraints in SocialAccountLinked model

--- src/django_social_auth_rest_project/urls.py
+++ src/django_social_auth_rest_project/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="docs", permanent=False)),
     path("admin/", admin.site.urls),
     path("api/social-auth/", include("django_social_auth_rest.urls")),
     path("api/social-auth/", include("django_social_auth_rest.urls.github")),
@@ -13,6 +15,6 @@ urlpatterns = [
     path(
         "api/docs/",
         SpectacularSwaggerView.as_view(url_name="schema"),
-        name="swagger-ui",
+        name="docs",
     ),
 ]
