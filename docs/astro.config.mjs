816988757ed755ab74bca082d10e3f2765d8e4dd
Kumar Sahil <krsahil8825@gmail.com>
2026-06-17 02:56:18 +0530

    add landing page and config astro settings and change theme

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,26 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightThemeGalaxy from "starlight-theme-galaxy";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Django Social Auth Rest",
+      plugins: [starlightThemeGalaxy()],
+      social: [{ icon: "github", label: "GitHub", href: "https://github.com/krsahil8825/django-social-auth-rest" }],
+      sidebar: [{ label: "Guides", items: [{ autogenerate: { directory: "guides" } }], collapsed: true }],
+    }),
+  ],
 });
---
+++ docs/src/content/docs/guides/01getting-started.md
@@ -0,0 +1,10 @@
+---
+title: Getting Started
+description: A step-by-step guide to setting up Django Social Auth Rest in your Django project, including provider-specific configuration and settings.
+sidebar:
+  order: 1
+slug: guides/getting-started
+---
+
+In this guide, we'll walk you through the process of setting up Django Social Auth Rest in your Django project. By the end of this guide, you'll have a working social authentication system integrated with your Django application.
+
